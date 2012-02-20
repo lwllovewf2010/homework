2295f95e9fe774f69c1b19aac64fde69123801b7
Merck Hung <merckhung@gmail.com>
2012-02-21 01:54:13 +0800

    Add compl2 calculation

--- endian/endian/endian.c
+++ endian/endian/endian.c
@@ -7,16 +7,30 @@
 //
 #include <stdio.h>
 
+
+int compl2( int val );
+
+
+int compl2( int val ) {
+
+    unsigned int tmp = (unsigned int)val;
+    
+    tmp = ~tmp + 1;
+    return (int)tmp;
+}
+
+
 int main (int argc, const char * argv[])
 {
     unsigned short val = 0x1234;
-    unsigned char *p = (char *)&val;
+    unsigned char *p = (unsigned char *)&val;
+    unsigned char a = 31, b = 14, c, sum = 0;
     
-    if( *p = 0x12 && *(p + 1) == 0x34 ) {
+    if( *p == 0x12 && *(p + 1) == 0x34 ) {
     
         printf( "BIG ENDIAN\n" );
     }
-    else if( *p = 0x34 && *(p + 1) == 0x12 ) {
+    else if( *p == 0x34 && *(p + 1) == 0x12 ) {
     
         printf( "LITTLE ENDIAN\n" );
     }
@@ -25,6 +39,13 @@ int main (int argc, const char * argv[])
         printf( "Internal Error\n" );
     }
     
+    for( c = a ^ b ; c != 0 ; c >>= 1 )
+        sum += (c & 1);
+    
+    printf( "Require bits = %d\n", sum );
+    
+    printf( "compl2(O) = %d\n", 0xF2 );
+    printf( "compl2(C) = %d\n", compl2( 0xF2 ) );
     
     return 0;
 }
