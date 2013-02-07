a15e3327c4ccb875880c38afb8b9b3e740e53070
Hung, Merck <merck.hung@intel.com>
2013-02-07 17:58:35 +0800

    Update.

--- kaleidoscope/kal.cpp
+++ kaleidoscope/kal.cpp
@@ -2,6 +2,31 @@
 
 using namespace llvm;
 
+
+// gettok - Return the next token from standard input.
+static int gettok() {
+
+	static int LastChar = ' ';
+
+	// Skip any
+	while( isspace( LastChar ) )
+		LastChar = getChar();
+
+	if( isalpha( LastChar ) ) {
+
+		IdentifierStr = LastChar;
+
+		while( isalnum( (LastChar = getchar()) ) )
+			IdentifierStr += LastChar;
+
+		if( IdentifierStr == "def" ) return tok_def;
+		if( IdentifierStr == "extern" ) return tok_extern;
+
+		return tok_identifier;
+	}
+}
+
+
 int main( int argc, char **argv ) {
 
 	return 0;
