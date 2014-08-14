c3d44e8ec09f87c76e1848f1de2254a13b0b5f12
Hung, Merck <merck.hung@intel.com>
2014-08-14 09:00:12 +0800

    Updated.

---
+++ cpptest/functor.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+struct add_x {
+
+	add_x(int x) : x(x) {
+
+	}
+	int operator()(int y) {
+
+		return x + y;
+	}
+
+private:
+	int x;
+};
+
+
+int main(int argc, char **argv) {
+
+	add_x add_48( 48 );
+	int i = add_48( 192 );
+
+	cout << i << "\n";
+
+	return 0;
+}
+
+
