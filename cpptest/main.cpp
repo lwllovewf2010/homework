063a34f2925f9d208109a2b5726f30a928bf7335
Hung, Merck <merck.hung@intel.com>
2015-01-04 16:31:48 +0800

    Updated cpptest and added zpipe

---
+++ cpptest/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "test.h"
+
+int main() {
+	std::cout << "TEST_CONSTANT = " << abc::Test::TEST_CONSTANT << "\n";
+	return 0;
+}
+
---
+++ cpptest/test.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+namespace abc {
+class Test {
+ public:
+  static const int TEST_CONSTANT = 1;
+};
+}
+
